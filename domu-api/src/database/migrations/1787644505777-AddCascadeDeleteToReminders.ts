d781af870a2eff73ab26a779c6f432e59ae32638
EmilioJasso19 <emiliojassodev@gmail.com>
2026-09-02 02:04:17 -0600

    fix(migration): remove reminder_sent column from task_occurrences in cascade delete migration

--- domu-api/src/database/migrations/1787644505777-AddCascadeDeleteToReminders.ts
+++ domu-api/src/database/migrations/1787644505777-AddCascadeDeleteToReminders.ts
@@ -5,13 +5,11 @@ export class AddCascadeDeleteToReminders1787644505777 implements MigrationInterf
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "reminders" DROP CONSTRAINT "FK_cb6709a939ba5d7080a6d4cc433"`);
-        await queryRunner.query(`ALTER TABLE "task_occurrences" DROP COLUMN "reminder_sent"`);
         await queryRunner.query(`ALTER TABLE "reminders" ADD CONSTRAINT "FK_cb6709a939ba5d7080a6d4cc433" FOREIGN KEY ("occurrence_id") REFERENCES "task_occurrences"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "reminders" DROP CONSTRAINT "FK_cb6709a939ba5d7080a6d4cc433"`);
-        await queryRunner.query(`ALTER TABLE "task_occurrences" ADD "reminder_sent" boolean NOT NULL DEFAULT false`);
         await queryRunner.query(`ALTER TABLE "reminders" ADD CONSTRAINT "FK_cb6709a939ba5d7080a6d4cc433" FOREIGN KEY ("occurrence_id") REFERENCES "task_occurrences"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
